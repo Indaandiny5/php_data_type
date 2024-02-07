78b3f55442e4f2453a3489bbd9e3d97e2a89b2b6
indaandiny5 <2223598.inda@smkn-2sbg.sch.id>
2024-02-07 09:21:49 +0700

    php_data_type

---
+++ integer.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    th{
+        background-color: rgb(82, 124, 165) ;
+    }
+</style>
+<body>
+    <h1 align="center">Biodata Diri</h1>
+
+    <table width="800px" height="200" border="1px" align="center" celpadding="5">
+        <tr>
+            <th> Photo </th>
+            <th colspan="2">Identitas Diri</th>
+        
+        </tr>
+
+        <tr>
+            <td rowspan="7">
+                <img src="inda6.jpg" width="200" height="200">
+
+            </td>
+            <td>
+                Nama Lengkap
+            </td>
+            <td> INDA ANDINY</td>
+        </tr>
+
+        <tr>
+            <td>
+                Tempat Tanggal Lahir
+            </td>
+            <td>Subang, 05 Desember 2006</td>
+        </tr>
+
+        <tr>
+            <td>
+                Alamat
+            </td>
+            <td>Jl.Raya Blok Tugu Utara, Rt/14 Rw/05, Desa Kalijati Barat, Kecamatan Kalijati</td>
+        </tr>
+
+        <tr>
+            <td>
+                Agama
+            </td>
+            <td>Islam</td>
+        </tr>
+
+        <tr>
+            <td>
+                Hobi
+            </td>
+            <td>Membaca</td>
+        </tr>
+
+        <tr>
+            <td>
+                Jenis Kelamin
+            </td>
+            <td>Perempuan</td>
+        </tr>
+    </table>
+<body>
+
+<center>
+<?php  
+$x = 5985;
+var_dump($x);
+?>  
+</center>
+</body>
+</html>
